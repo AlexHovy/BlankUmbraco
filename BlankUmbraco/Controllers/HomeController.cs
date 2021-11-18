85b30a1f3d693acaeab7c51d8998ed16ed15d32e
Alex Hovy <alex@syncrony.com>
2021-11-18 19:09:07 +0200

    Home controller, model, views, scripts and css working.

---
+++ BlankUmbraco/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using BlankUmbraco.Models;
+
+namespace BlankUmbraco
+{
+    public class HomeController : RenderController
+    {
+        private readonly IVariationContextAccessor _variationContextAccessor;
+        private readonly ServiceContext _serviceContext;
+        public HomeController(ILogger<HomeController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor, IVariationContextAccessor variationContextAccessor, ServiceContext context)
+            : base(logger, compositeViewEngine, umbracoContextAccessor)
+        {
+            _variationContextAccessor = variationContextAccessor;
+            _serviceContext = context;
+        }
+
+        public override IActionResult Index()
+        {
+
+            // you are in control here!
+            // create our ViewModel based on the PublishedContent of the current request:
+            // set our custom properties
+            var homeModel = new HomeViewModel(CurrentPage, new PublishedValueFallback(_serviceContext, _variationContextAccessor))
+            {
+                Test = "Hello World"
+            };
+
+
+            // return our custom ViewModel
+            return CurrentTemplate(homeModel);
+
+        }
+    }
+}
---
+++ BlankUmbraco/Models/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace BlankUmbraco.Models
+{
+    public class HomeViewModel : PublishedContentWrapped
+    {
+        // The PublishedContentWrapped accepts an IPublishedContent item as a constructor
+        public HomeViewModel(IPublishedContent content, IPublishedValueFallback publishedValueFallback) : base(content, publishedValueFallback)
+        {
+        }
+
+        // Custom properties here...
+        public string Test { get; set; }
+    }
+}
